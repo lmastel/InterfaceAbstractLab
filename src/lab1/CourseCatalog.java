b996f5e52c7b021a615ca889b88727aea1ec65cc
lmastel@my.wctc.edu <>
2013-01-30 21:36:51 -0600

    Commit 1

---
+++ src/lab1/CourseCatalog.java
@@ -0,0 +1,120 @@
+
+package lab1;
+
+import javax.swing.JOptionPane;
+
+
+public abstract class CourseCatalog {
+    private String courseName;
+    private double credits;
+    
+    private String courseNumber;
+    private String prerequisites;
+
+    //IntroToProgrammingCourse- set by constructor and by the following code
+    //which validates courseName which is best practice
+    public final void setCourseName(String courseName) {
+        if(courseName == null || courseName.length() == 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: courseName cannot be null of empty string");
+            System.exit(0);
+        }
+        this.courseName = courseName;
+    }
+    //IntroJavaCourse- set by constructor, no setter
+    
+    //AdvancedJavaCourse - set by constructor and by the following code
+    //which doesn't validate courseName
+//    public void setCourseName(String courseName) {
+//        this.courseName = courseName;
+//    }
+    
+    //IntroToProgrammingCourse - will use this one for all subclasses and make final
+    public final String getCourseName() {
+        return courseName;
+    }
+    //IntroJavaCourse- missing
+    //AdvancedJavaCourse-
+//    public String getCourseName() {
+//        return courseName;
+//    }
+    
+    
+    //IntroToProgramming-set by constructor and by the following code
+    //validates courseNumber which is best practice
+    public final void setCourseNumber(String courseNumber) {
+        if(courseNumber == null || courseNumber.length() == 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: courseNumber cannot be null of empty string");
+            System.exit(0);
+        }
+        this.courseNumber = courseNumber;
+    }
+    //IntroJavaCourse-wset by constructor and by the following code
+    //doesn't validate courseNumber so will not be used
+//    public void setCourseNumber(String courseNumber) {
+//        this.courseNumber = courseNumber;
+//    }
+    //AdvancedJavaCourse-set by constructor and by the following code
+    //doesn't validate courseNumber so will not be used
+//    public void setCourseNumber(String courseNumber) {
+//        this.courseNumber = courseNumber;
+//    }
+    
+    //IntroToProgrammingCourse- will move this getter to superclass and remove
+    //from subclasses and also will make final
+    public final String getCourseNumber() {
+        return courseNumber;
+    }
+    //IntroJavaCourse-
+//    public String getCourseNumber() {
+//        return courseNumber;
+//    }
+    //AdvancedJavaCourse-
+//    public String getCourseNumber() {
+//        return courseNumber;
+//    }
+    
+    
+    //setCredits method- I will make abstract because implementation differs
+    //in subclasses and logic errors
+    public abstract void setCredits(double credits);
+    //IntroToProgrammingCourse- 
+//    public void setCredits(double credits) {
+//        if(credits < 0.5 || credits > 4.0) {
+//            JOptionPane.showMessageDialog(null,
+//                    "Error: credits must be in the range 0.5 to 4.0");
+//            System.exit(0);
+//        }
+//        this.credits = credits;
+//    }
+    //IntroJavaCourse-logic conflicts with error message,
+//    public void setCredits(double credits) {
+//        if(credits < 0 || credits > 5.0) {
+//            System.out.println(
+//                    "Error: credits must be in the range 0.5 to 4.0");
+//            System.exit(0);
+//        }
+//        this.setCredits(credits);
+//    }
+    //AdvancedJavaCourse-
+//    public void setCredits(double credits) {
+//        if(credits < 0.5 || credits > 4.0) {
+//            JOptionPane.showMessageDialog(null,
+//                    "Error: credits must be in the range 0.5 to 4.0");
+//            System.exit(0);
+//        }
+//        this.setCredits(credits);
+//    }
+    
+    //IntroToProgrammingCourse-will move from subclasses to superclass and
+    //make final
+    public final double getCredits() {
+        return credits;
+    }
+    //IntroJavaCourse-
+//    public double getCredits() {
+//        return credits;
+//    }
+    //AdvancedJavaCourse- missing getCredits()
+}
---
+++ src/lab1/IntroJavaCourse.java
@@ -0,0 +1,57 @@
+package lab1;
+
+/**
+ * Describe responsibilities here.
+ *
+ * @author      your name goes here
+ * @version     1.00
+ */
+public class IntroJavaCourse extends CourseCatalog{
+    String courseName;
+    private String courseNumber;
+    private double credits;
+    private String prerequisites;
+
+    public IntroJavaCourse(String courseName, String courseNumber) {
+        this.courseName = courseName;
+        this.courseNumber = courseNumber;
+    }
+
+    //moved to superclass
+//    public String getCourseNumber() {
+//        return courseNumber;
+//    }
+
+    //moved to superclass
+//    public void setCourseNumber(String courseNumber) {
+//        this.courseNumber = courseNumber;
+//    }
+
+    //moved to superclass
+//    public double getCredits() {
+//        return credits;
+//    }
+
+
+    public String getPrerequisites() {
+        return prerequisites;
+    }
+
+    public void setPrerequisites(String prerequisites) {
+        this.prerequisites = prerequisites;
+    }
+
+    //implements abstract method from CoursCatalog    
+    public void setCredits(double credits) {
+        //correction of logic to match error message after validating with
+        //admissions and computer science department    
+        //if(credits < 0 || credits > 5.0) {
+          if(credits < 0.5 || credits > 4.0){  
+            System.out.println(
+                    "Error: credits must be in the range 0.5 to 4.0");
+            System.exit(0);
+        }
+        this.setCredits(credits);
+    }
+
+}
